9f363b3fbe81f1e3dabd654eb0c9fe66a53c99e7
Axtemka <115342104+Axtemka@users.noreply.github.com>
2026-03-13 11:09:10 +0300

    Added functional to file readers (JSON, XML, TXT)

--- src/main/java/com/mycompany/lab1/Lab1.java
+++ src/main/java/com/mycompany/lab1/Lab1.java
@@ -4,6 +4,12 @@
 
 package com.mycompany.lab1;
 
+import com.mycompany.lab1.data.Mission;
+import com.mycompany.lab1.readers.JsonReader;
+import com.mycompany.lab1.readers.TextReader;
+import com.mycompany.lab1.readers.XmlReader;
+import java.net.URI;
+
 /**
  *
  * @author temdo
@@ -11,7 +17,13 @@ package com.mycompany.lab1;
 public class Lab1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("Netbeans super cool");
+//       JsonReader j = new JsonReader();
+//        XmlReader x = new XmlReader();
+//        TextReader t = new TextReader();
+        
+//        Mission m = t.getMissionInfo("C:\\Users\\temdo\\Downloads\\Mission Data\\var2\\Mission B.txt");
+//        m.info();
+//        String property = System.getProperty("file.encoding");
+//        System.out.println(property);
     }
 }
